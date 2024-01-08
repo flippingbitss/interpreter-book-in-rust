dae99f1cea4121e990506542cb2e668b5964264b
Manpreet Matharu <matharumanpreet@gmail.com>
2024-01-08 00:05:44 -0500

    add conditionals eval

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -6,18 +6,30 @@ use crate::{
 pub fn eval_program(prog: Program) -> Result<Object, &str> {
     let mut result = Ok(Object::Null);
     for stmt in prog.stmts {
-        if let Stmt::Expr { expr } = stmt {
-            result = eval(expr);
-        }
+        result = eval_stmt(stmt);
     }
     result
+}
+
+fn eval_stmt(stmt: Stmt<'_>) -> Result<Object, &str> {
+    match stmt {
+        Stmt::Expr { expr } => eval(expr),
+        Stmt::Block { stmts, .. } => {
+            let mut result = Object::Null;
+            for stmt in stmts {
+                result = eval_stmt(stmt)?
+            }
+            Ok(result)
+        }
+        _ => Ok(Object::Null),
+    }
 }
 
 fn eval(expr: Expr) -> Result<Object, &str> {
     match expr {
         //Expr::Identifier { token, value } => todo!(),
-        Expr::IntLiteral { value, .. } => Ok(Object::Integer { value }),
-        Expr::BoolLiteral { value, .. } => Ok(Object::Bool { value }),
+        Expr::IntLiteral { value, .. } => Ok(Object::Integer(value)),
+        Expr::BoolLiteral { value, .. } => Ok(Object::Bool(value)),
         //Expr::FnLiteral { token, parameters, block } => todo!(),
         //Expr::Call { token, function, arguments } => todo!(),
         Expr::Prefix { op, expr, .. } => {
@@ -27,19 +39,49 @@ fn eval(expr: Expr) -> Result<Object, &str> {
         Expr::Infix {
             left, op, right, ..
         } => eval_infix_expr(op, eval(*left)?, eval(*right)?),
-        //Expr::If { token, condition, consequence, alternative } => todo!(),
+        Expr::If {
+            condition,
+            consequence,
+            alternative,
+            ..
+        } => eval_conditional_expr(*condition, *consequence, alternative.map(|s| *s)),
         _ => Err("not supported expr type"),
+    }
+}
+
+fn eval_conditional_expr<'a>(
+    condition: Expr<'a>,
+    consequence: Stmt<'a>,
+    alternative: Option<Stmt<'a>>,
+) -> Result<Object, &'a str> {
+    let cond = eval(condition)?;
+    match cond {
+        Object::Bool(value) => {
+            if value {
+                println!("bool is true");
+                eval_program(Program {
+                    stmts: vec![consequence],
+                })
+            } else if alternative.is_some() {
+                eval_program(Program {
+                    stmts: vec![alternative.unwrap()],
+                })
+            } else {
+                Ok(Object::Null)
+            }
+        }
+        _ => return Err("conditional expression isn't a boolean"),
     }
 }
 
 fn eval_prefix_expr(op: &[u8], right: Object) -> Result<Object, &str> {
     match op {
         b"!" => match right {
-            Object::Bool { value } => Ok(Object::Bool { value: !value }),
+            Object::Bool(value) => Ok(Object::Bool(!value)),
             _ => Err("operator '!' only applies to boolean types"),
         },
         b"-" => match right {
-            Object::Integer { value } => Ok(Object::Integer { value: -value }),
+            Object::Integer(value) => Ok(Object::Integer(-value)),
             _ => Err("operator '-' only applies to numbers"),
         },
         _ => Err("operator not supported"),
@@ -48,17 +90,23 @@ fn eval_prefix_expr(op: &[u8], right: Object) -> Result<Object, &str> {
 
 fn eval_infix_expr(op: &[u8], left: Object, right: Object) -> Result<Object, &str> {
     match (left, right) {
-        (Object::Integer { value: left }, Object::Integer { value: right }) => {
-            Ok(Object::Integer {
-                value: match op {
-                    b"*" => left * right,
-                    b"-" => left - right,
-                    b"+" => left + right,
-                    b"/" => left / right,
-                    _ => return Err("not supported"),
-                },
-            })
-        }
+        (Object::Integer(left), Object::Integer(right)) => Ok(match op {
+            b"*" => Object::Integer(left * right),
+            b"-" => Object::Integer(left - right),
+            b"+" => Object::Integer(left + right),
+            b"/" => Object::Integer(left / right),
+            b"<" => Object::Bool(left < right),
+            b">" => Object::Bool(left > right),
+            b"==" => Object::Bool(left == right),
+            b"!=" => Object::Bool(left != right),
+            _ => return Err("not supported"),
+        }),
+        (Object::Bool(left), Object::Bool(right)) => Ok(match op {
+            b"==" => Object::Bool(left == right),
+            b"!=" => Object::Bool(left != right),
+            _ => return Err("not supported"),
+        }),
+
         _ => Err("operand can only be applied to numbers"),
     }
 }
@@ -81,7 +129,7 @@ mod tests {
     }
 
     fn assert_int_obj(obj: &Object, evalue: i64) {
-        if let Object::Integer { value } = obj {
+        if let Object::Integer(value) = obj {
             assert_eq!(*value, evalue)
         } else {
             panic!("not an integer object")
@@ -89,7 +137,7 @@ mod tests {
     }
 
     fn assert_bool_obj(obj: &Object, evalue: bool) {
-        if let Object::Bool { value } = obj {
+        if let Object::Bool(value) = obj {
             assert_eq!(*value, evalue)
         } else {
             panic!("not an boolean object")
@@ -123,7 +171,27 @@ mod tests {
 
     #[test]
     fn test_bool_expr() {
-        let inputs = [("true", true), ("false", false)];
+        let inputs = [
+            ("true", true),
+            ("false", false),
+            ("1 < 2", true),
+            ("1 > 2", false),
+            ("1 < 1", false),
+            ("1 > 1", false),
+            ("1 == 1", true),
+            ("1 != 1", false),
+            ("1 == 2", false),
+            ("1 != 2", true),
+            ("true == true", true),
+            ("false == false", true),
+            ("true == false", false),
+            ("true != false", true),
+            ("false != true", true),
+            ("(1 < 2) == true", true),
+            ("(1 < 2) == false", false),
+            ("(1 > 2) == true", false),
+            ("(1 > 2) == false", true),
+        ];
         for (i, expected) in inputs {
             let obj = eval_prog(i).unwrap();
             assert_bool_obj(&obj, expected);
@@ -149,6 +217,28 @@ mod tests {
         let neg_input = ["!5", "!!5"];
         for i in neg_input {
             assert!(eval_prog(&i).is_err());
+        }
+    }
+
+    #[test]
+    fn test_conditional_expr() {
+        let truthy_inputs = [
+            ("if (true) { 10 }", 10),
+            ("if (1 < 2) { 10 }", 10),
+            ("if (1 > 2) { 10 } else { 20 }", 20),
+            ("if (1 < 2) { 10 } else { 20 }", 10),
+        ];
+        let falsy_inputs = ["if (false) { 10 }", "if (1 > 2) { 10 }"];
+
+        for (input, evalue) in truthy_inputs {
+            let value = eval_prog(input).unwrap();
+            eprintln!("{} {}", input, value);
+            assert_int_obj(&value, evalue);
+        }
+
+        for input in falsy_inputs {
+            let value = eval_prog(input);
+            assert!(matches!(value.unwrap(), Object::Null));
         }
     }
 }
--- src/object.rs
+++ src/object.rs
@@ -1,22 +1,16 @@
 use core::fmt;
 
-
-
 pub enum Object {
-    Integer {
-        value: i64
-    },
-    Bool {
-        value: bool
-    },
-    Null 
+    Integer(i64),
+    Bool(bool),
+    Null,
 }
 
 impl fmt::Display for Object {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Object::Integer { value } => write!(f, "{}", value),
-            Object::Bool { value } => write!(f, "{}", value),
+            Object::Integer(value) => write!(f, "{}", value),
+            Object::Bool(value) => write!(f, "{}", value),
             Object::Null => write!(f, "nil"),
         }
     }
